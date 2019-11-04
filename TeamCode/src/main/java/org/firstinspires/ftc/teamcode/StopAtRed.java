6c29ad29ac8ad0dcde79e2232ac51a6a7a1e1b1a
Team10477 <Fall#2019>
2019-11-03 19:57:29 -0500

    Stop At Red

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StopAtRed.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous(name="Stop At Red")
+
+public class StopAtRed extends LinearOpMode {
+
+    HardwarePushbot robot = new HardwarePushbot();
+
+    RevColorSensorV3 colorSensor = null;    // Hardware Device Object
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        colorSensor = hardwareMap.get(RevColorSensorV3.class, "color_sensor");
+
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F, 0F, 0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        boolean colorFound = false;
+
+        final View relativeLayout = ((Activity)hardwareMap.appContext).findViewById(R.id.RelativeLayout);
+
+        colorSensor.enableLed(true);
+
+        waitForStart();
+
+       while (colorFound == false) {
+            Color.RGBToHSV((int)(colorSensor.red() * 8), (int)(colorSensor.green() *8), (int)(colorSensor.blue() * 8), hsvValues);
+
+            float hue = hsvValues[0];
+
+            telemetry.addData("Color Red", hue);
+            telemetry.update();
+
+            boolean redHue = hue < 60 || hue > 320;
+            boolean blueHue = hue > 120 && hue < 260;
+
+            if (redHue)  {
+               robot.setWheelPower(0);
+               colorFound = true;
+           }else {
+                robot.setWheelPower(0.5);
+                }
+            }
+       }
+
+
+
+    }
