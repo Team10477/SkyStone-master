45e28bc660d505e41858737ac882346afbf0f615
Team10477 <Fall#2019>
2019-11-14 00:24:28 -0500

    Skystone failure mode timer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneIdentification.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneIdentification.java
@@ -30,6 +30,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -127,6 +128,8 @@ public class SkyStoneIdentification  {
     private OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia = null;
 
+    ElapsedTime elapsedTime = new ElapsedTime();
+
     /**
      * This is the webcam we are to use. As with other hardware devices such as motors and
      * servos, this device is identified using the robot configuration tool in the FTC application.
@@ -146,37 +149,72 @@ public class SkyStoneIdentification  {
 
      //   loadVuforiaTrackables() ;
         targetVisible = false;
-       while (!targetVisible) {
+        VectorF translation = null;
+        elapsedTime.reset();
+        while (!targetVisible && elapsedTime.seconds() < 8) {
            if (isRed)
                robot.setWheelPowerForSideWithDelta(0.15, 1.1);
            else
                robot.setWheelPowerForSideWithDelta(-0.15, 1.1);
             // check all the trackable targets to see which one (if any) is visible.
-            targetVisible = false;
+
+            VuforiaTrackable trackTarget = null;
             for (VuforiaTrackable trackable : allTrackables) {
                 if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
                    if (trackable.getName().equals("Stone Target")) {
                         telemetry.addData("Visible Target", trackable.getName());
                         targetVisible = true;
+                        trackTarget = trackable;
 
                     }
 
                     // getUpdatedRobotLocation() will return null if no new information is available since
                     // the last time that call was made, or if the trackable is not currently visible.
-                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackTarget.getListener()).getUpdatedRobotLocation();
                     if (robotLocationTransform != null) {
                         lastLocation = robotLocationTransform;
                     }
-                    break;
+                    //break;
                 }
             }
 
             // Provide feedback as to where the robot is located (if we know).
             if (targetVisible) {
+
                 // express position (translation) of robot in inches.
-                VectorF translation = lastLocation.getTranslation();
-                telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+                translation = lastLocation.getTranslation();
+
+             /*   double x = translation.get(0);
+                double y = translation.get(1); */
+
+                while (translation.get(1) < 6.0 && lastLocation != null ) {
+                    telemetry.addData("Pos (in) Y value", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                            translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+                    telemetry.update();
+                    robot.setWheelPowerForSide(0.15);
+                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackTarget.getListener()).getUpdatedRobotLocation();
+                    if (robotLocationTransform != null) {
+                        lastLocation = robotLocationTransform;
+                    }
+                    translation = lastLocation.getTranslation();
+                }
+                translation = lastLocation.getTranslation();
+                robot.setWheelDirectionReverse(); // Go Forward near the block
+
+                elapsedTime.reset();
+                while (translation.get(0) < -2.0 && lastLocation != null && elapsedTime.seconds() < 2.0) {
+                    telemetry.addData("Pos (in) X values", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                            translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+                    telemetry.update();
+                    robot.setWheelPower(0.15);
+                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackTarget.getListener()).getUpdatedRobotLocation();
+                    if (robotLocationTransform != null) {
+                        lastLocation = robotLocationTransform;
+                    }
+                    translation = lastLocation.getTranslation();
+                }
+
+                robot.stopWheels();
 
                 // express the rotation of the robot in degrees.
                 Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
@@ -188,6 +226,7 @@ public class SkyStoneIdentification  {
             telemetry.update();
         }
 
+        robot.stopWheels();
         // Disable Tracking when we are done;
         targetsSkyStone.deactivate();
 
