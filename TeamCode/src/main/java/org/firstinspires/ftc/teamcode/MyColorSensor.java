565e529b6cac8229678a287edb2d51f5edfeb3a1
Team10477 <Fall#2019>
2019-12-12 00:06:23 -0500

    Stop At Red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyColorSensor.java
@@ -31,6 +31,7 @@ public class MyColorSensor {
     public void strafeToGivenColor(LinearOpMode opMode, RevColorSensorV3 colorSensor,  HardwarePushbot robot, MyColor myColor, double power ) {
         boolean colorFound = false;
         while (colorFound == false && opMode.opModeIsActive()) {
+
             Color.RGBToHSV((int)(colorSensor.red() * 8), (int)(colorSensor.green() *8), (int)(colorSensor.blue() * 8), hsvValues);
 
             float hue = hsvValues[0];
@@ -47,7 +48,7 @@ public class MyColorSensor {
 
             boolean yellow = hue <100 && saturation > 0.6 && value > 10;
 
-            if ((myColor == MyColor.RED && redHue )  || (myColor == MyColor.BLUE && blueHue) || (myColor == MyColor.BLACK && blackHue)){
+            if ((myColor == MyColor.RED && redHue )  || (myColor == MyColor.BLUE && blueHue) || (myColor == MyColor.BLACK && blackHue)) {
                 robot.stopWheels();
                 colorFound = true;
             }else {
@@ -56,34 +57,47 @@ public class MyColorSensor {
         }
     }
 
+    private boolean calculateColor(Telemetry telemetry, RevColorSensorV3 colorSensorV3, MyColor myColor) {
+        Color.RGBToHSV((int)(colorSensorV3.red() * 8), (int)(colorSensorV3.green() *8), (int)(colorSensorV3.blue() * 8), hsvValues);
+
+        float hue = hsvValues[0];
+
+        float saturation = hsvValues[1];
+
+        float value = hsvValues[2];
+
+        telemetry.addData("black hue top ", hue);
+        telemetry.addData("black saturation top ", saturation);
+        telemetry.addData("black value top", value);
+
+        boolean redHue = (hue < 60 || hue > 320) && (saturation > 0.5);
+
+        boolean blueHue = (hue > 180 && hue < 240) && (saturation > 0.5);
+
+        boolean blackHue = hue > 100 && saturation < 0.6 && value > 8 ;
+
+        boolean yellow = hue <100 && saturation > 0.6 && value > 10;
+
+        telemetry.addData("black hue", hue);
+        telemetry.addData("black saturation", saturation);
+        telemetry.addData("black value", value);
+        telemetry.update();
+
+        if ((myColor == MyColor.RED && redHue )  || (myColor == MyColor.BLUE && blueHue) || (myColor == MyColor.BLACK && blackHue))
+            return true;
+        else
+            return false;
+
+    }
+
     public void strafeToColorByTime(Telemetry telemetry, LinearOpMode opMode, RevColorSensorV3 colorSensor,  HardwarePushbot robot, MyColor myColor, double power, FeedbackMovement feedbackMovement ) {
         boolean colorFound = false;
         elapsedTime.reset();
         feedbackMovement.initIntegralError(power, robot);
 
         while (colorFound == false && elapsedTime.seconds() < 4 && opMode.opModeIsActive()) {
-            Color.RGBToHSV((int)(colorSensor.red() * 8), (int)(colorSensor.green() *8), (int)(colorSensor.blue() * 8), hsvValues);
 
-            float hue = hsvValues[0];
-
-            float saturation = hsvValues[1];
-
-            float value = hsvValues[2];
-
-            boolean redHue = (hue < 60 || hue > 320) && (saturation > 0.5);
-
-            boolean blueHue = (hue > 180 && hue < 240) && (saturation > 0.5);
-
-            boolean blackHue = hue > 100 && saturation < 0.6 && value > 8 ;
-
-            boolean yellow = hue <100 && saturation > 0.6 && value > 10;
-
-            telemetry.addData("black hue top ", hue);
-            telemetry.addData("black saturation top ", saturation);
-            telemetry.addData("black value top", value);
-
-
-            if ((myColor == MyColor.RED && redHue )  || (myColor == MyColor.BLUE && blueHue) || (myColor == MyColor.BLACK && blackHue)){
+            if  (calculateColor(telemetry, colorSensor, myColor)) {
                 robot.stopWheels();
                 colorFound = true;
             }else {
@@ -98,32 +112,7 @@ public class MyColorSensor {
         elapsedTime.reset();
         feedbackMovement.initIntegralError(power, robot);
         while (colorFound == false && opMode.opModeIsActive()) {
-            Color.RGBToHSV((int)(colorSensor.red() * 8), (int)(colorSensor.green() *8), (int)(colorSensor.blue() * 8), hsvValues);
-
-            float hue = hsvValues[0];
-
-            float saturation = hsvValues[1];
-
-            float value = hsvValues[2];
-
-            telemetry.addData("black hue top ", hue);
-            telemetry.addData("black saturation top ", saturation);
-            telemetry.addData("black value top", value);
-
-            boolean redHue = (hue < 60 || hue > 320) && (saturation > 0.5);
-
-            boolean blueHue = (hue > 180 && hue < 240) && (saturation > 0.5);
-
-            boolean blackHue = hue > 100 && saturation < 0.6 && value > 8 ;
-
-            boolean yellow = hue <100 && saturation > 0.6 && value > 10;
-
-            telemetry.addData("black hue", hue);
-            telemetry.addData("black saturation", saturation);
-            telemetry.addData("black value", value);
-            telemetry.update();
-
-            if ((myColor == MyColor.RED && redHue )  || (myColor == MyColor.BLUE && blueHue) || (myColor == MyColor.BLACK && blackHue)){
+            if  (calculateColor(telemetry, colorSensor, myColor)) {
                 robot.stopWheels();
                 colorFound = true;
             }else {
@@ -139,36 +128,10 @@ public class MyColorSensor {
         elapsedTime.reset();
         feedbackMovement.initIntegralError(power, robot);
         while (colorFound == false && opMode.opModeIsActive()) {
-            Color.RGBToHSV((int)(colorSensor.red() * 8), (int)(colorSensor.green() *8), (int)(colorSensor.blue() * 8), hsvValues);
-
-            float hue = hsvValues[0];
-
-            float saturation = hsvValues[1];
-
-            float value = hsvValues[2];
-
-            telemetry.addData("black hue top ", hue);
-            telemetry.addData("black saturation top ", saturation);
-            telemetry.addData("black value top", value);
-
-            boolean redHue = (hue < 60 || hue > 320) && (saturation > 0.5);
-
-            boolean blueHue = (hue > 180 && hue < 240) && (saturation > 0.5);
-
-            boolean blackHue = hue > 100 && saturation < 0.6 && value > 8 ;
-
-            boolean yellow = hue <100 && saturation > 0.6 && value > 10;
-
-            telemetry.addData("black hue", hue);
-            telemetry.addData("black saturation", saturation);
-            telemetry.addData("black value", value);
-            telemetry.update();
-
-            if ((myColor == MyColor.RED && redHue )  || (myColor == MyColor.BLUE && blueHue) || (myColor == MyColor.BLACK && blackHue)){
+            if  (calculateColor(telemetry, colorSensor, myColor)) {
                 robot.stopWheels();
                 colorFound = true;
             }else {
-                // robot.setWheelPowerForSide(power);
                 feedbackMovement.driveWithFeedback(robot,0,  power);
             }
 
